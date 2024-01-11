4b34dbb9a7b411e740ca6ab531c3be7a2b501d8d
Ricky Dodd <rickydoddmobile@gmail.com>
2024-01-11 20:17:37 +1100

    add: basic cli and file handling
    This commit adds a basic CLI, including a command to argue a filename
    and a disassembly flag, and the commit also adds file handling for
    opening the byte file.

--- src/8086emu.c
+++ src/8086emu.c
@@ -2,6 +2,28 @@
 
 int main(int argc, char *argv[])
 {
+    unsigned char buffer[2];
+
+    if (argc == 1) {
+        printf("Not enough arguments. Usage: 8086emu filename -d\n");
+        return 1;
+    } else if (argc > 3) {
+        printf("Too many arguments. Usage: 8086emu filename -d\n");
+        return 1;
+    }
+
+    // open bin file
+    printf("Attempting to open file: %s\n", argv[1]);
+    FILE *in_file = fopen(argv[1], "r");
+    if (in_file == NULL) {
+        printf("File could not be opened.\n");
+        return 1;
+    }
+    printf("File successfully opened.\n");
+
+    if (in_file != NULL) {
+        fclose(in_file);
+    }
     return 0;
 }
 
